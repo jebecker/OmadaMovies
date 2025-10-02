6f1c311e76f75db8c6b8c1799f15296345002581
Jayme Becker <jaymebecker@icloud.com>
2025-10-02 15:06:27 -0500

    Initial Commit

---
+++ OmadaMoviesTests/OmadaMoviesTests.swift
@@ -0,0 +1,16 @@
+//
+//  OmadaMoviesTests.swift
+//  OmadaMoviesTests
+//
+//  Created by Jayme Becker on 10/2/25.
+//
+
+import Testing
+
+struct OmadaMoviesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
